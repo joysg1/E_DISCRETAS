64dd7ab732c966fb6b4fb744c48e19dea606f7e0
joysg1 <joy.nelaton@utp.ac.pa>
2025-09-21 23:36:35 -0500

    Add files via upload

---
+++ LABS/LAB4/lab4_funciones.py
@@ -0,0 +1,39 @@
+
+def es_reflexiva(A, R):
+    pass
+
+def es_irreflexiva(A, R):
+    pass
+
+def es_simetrica(A, R):
+    pass
+
+def es_asimetrica(A, R):
+    pass
+
+def es_antisimetrica(A, R):
+    pass
+
+def es_transitiva(A, R):
+    pass
+
+def dominio(R):
+    pass
+
+def codominio(R):
+    pass
+
+def funcion_alea(S, T):
+    pass
+
+def definida_todas_partes(A, R):
+    pass
+
+def es_uno_a_uno(R):
+    pass
+
+def es_exhaustiva(A, R):
+    pass
+
+def es_biyectiva(A, R):
+    pass
